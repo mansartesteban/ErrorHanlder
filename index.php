f1404b2d1822a2b730ff088b2288f1da978c07ec
Esteban <esteban.mansart@gmail.com>
2019-03-10 23:58:46 +0100

    Parsing des logs
    Ajout des namespaces (gestion psr-4)
    Parsing des log sous forme d'objets (EHError)
    Gestion d'erreur : Création du répertoire de logs s'il n'existe pas

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Esteban
- * Date: 08/03/2019
- * Time: 22:51
- */
--- src/ErrorHandler/ErrorHandler.php
+++ src/ErrorHandler/ErrorHandler.php
@@ -2,13 +2,10 @@
 
 namespace ErrorHandler;
 
-use DateTime;
-use Exception;
-use http\Exception\RuntimeException;
-
+use ErrorHandler\EHError;
 /**
- * Classe de gestion de log d'erreur.\n
- * Visite https://github.com/mansartesteban/ErrorHandler pour consulter la documentation
+ * Classe de gestion de log d'erreur.
+ * Visitez https://github.com/mansartesteban/ErrorHandler pour consulter la documentation
  */
 class ErrorHandler {
     
@@ -16,19 +13,19 @@ class ErrorHandler {
     private static $options = [];
     private static $documentation = "Pour plus d'informations, veuillez consulter la documentation disponible sur : https://github.com/mansartesteban/ErrorHandler";
     
-    const START_DELIMITER = "##########";
-    const END_DELIMITER = "__________";
-    
-    
     /* ===== PUBLIC METHODS ===== */
-    
+
     /**
-     * @param $options Tableau de paramètres
+     * @param array $options Tableau de paramètres
      */
     public static function init(array $options = []) {
         self::$options = [
             "logDir" => "/log",
-            "formatDateFilename" => "Y-m-d",
+            "formats" => [
+                "dateFilename" => "Y-m-d", // Format de date pour le nom du fichier
+                "dateLog" => "Y-m-d H:i:s.u" // Format de date pour le log en lui-même
+            ],
+            "logSeparator" => ">_______________<",
             "fileExtension" => ".log"
         ];
         self::$options = $options + self::$options;
@@ -45,27 +42,33 @@ class ErrorHandler {
 
     /**
      * @param string $type Type d'erreur (Nom du dossier)
-     * @param Exception $error L'exception
+     * @param \Exception $error L'exception
      * @param mixed $additionalParams
-     * @throws Exception
+     * @throws \Exception
      */
-    public static function log($type = "", Exception $error = null, $additionalParams = null) {
+    public static function log($type = "", \Exception $error = null, $additionalParams = null) {
         if (self::$created) {
             if (null === $type || empty($type)) {
                 $type = "unknown";
             }
             if (null === $error) {
-                throw new RuntimeException("L'erreur passée en arguments est nulle");
+                throw new \RuntimeException("L'erreur passée en arguments est nulle");
             }
-            $now = new DateTime();
-            $logFilename = $now->format(self::$options["formatDateFilename"]) . self::$options["fileExtension"];
+            $now = new \DateTime();
+            $logFilename = $now->format(self::$options["formats"]["dateFilename"]) . self::$options["fileExtension"];
             $logDir = self::$options["logDir"];
-
+            if (!is_dir($logDir)) {
+                if (!mkdir($logDir)) {
+                    throw new \RuntimeException("Impossible de créer le répertoire de log, veuillez vérifier si vous avez les droits d'écriture suffisants");
+                }
+            }
             $dir = $logDir . "/" . strtolower($type) . "/";
             if (!is_dir($dir)) {
-                @mkdir($dir);
+                if (!mkdir($dir)) {
+                    throw new \RuntimeException("Impossible de créer le sous-répertoire de log \"" .strtolower($type) .  "\", veuillez vérifier si vous avez les droits d'écriture suffisants");
+                }
             }
-            file_put_contents($dir . $logFilename, self::formatError($error, $type, $additionalParams), FILE_APPEND);
+            file_put_contents($dir . $logFilename, self::formatError($error, $type, $now, $additionalParams), FILE_APPEND);
 
         } else {
             throw new \LogicException("La class n'a pas été initialisée. " . self::$documentation);
@@ -75,10 +78,10 @@ class ErrorHandler {
 
     /**
      * @param String $type Le dossier de log recherché
-     * @param DateTime $date La date du journal de log recherché
+     * @param \DateTime $date La date du journal de log recherché
      * @return string|false Retourne le contenu du fichier trouvé sous forme de chaîne de caractères, false si une erreur survient
      */
-    public static function getRawLog($type = "", DateTime $date = null) {
+    public static function getRawLog($type = "", \DateTime $date = null) {
         if (empty($type) || null === $type) {
             $type = (null === $type) ? ("null") : ("\"\"");
             throw new \InvalidArgumentException("Le type doit être renseigné, type: " . $type);
@@ -89,15 +92,15 @@ class ErrorHandler {
         
         $subDir = self::$options["logDir"] . "/" . $type;
         if (is_dir($subDir)) {
-            $filename = $subDir . "/" . $date->format(self::$options["formatDateFilename"]) . self::$options["fileExtension"];
-            if (file_exists()) {
+            $filename = $subDir . "/" . $date->format(self::$options["formats"]["dateFilename"]) . self::$options["fileExtension"];
+            if (file_exists($filename)) {
                 if ($log = file_get_contents($filename)) {
                     return $log;
                 } else {
                     throw new \RuntimeException("Le fichier n'a pas été lu correctement");
                 }
             } else {
-                throw new \RuntimeException("Le fichier spécifié n'existe pas");
+                return false; // Il n'y a pas d'erreur, juste le fichier n'existe pas;
             }
         } else {
             throw new \RuntimeException("Le dossier spécifié n'existe pas");
@@ -109,21 +112,20 @@ class ErrorHandler {
     /**
      * @param string $raw Le format brut reçu de ErrorHandler::getRawLog()
      * @return array|false Tableau des logs traités et formattés
+     * @throws \Exception
      */
     public static function parseRawLog($raw = "") {
-        /* TODO: return object
-         * ->DATE
-         * ->HOST
-         * ->TYPE
-         * ->FILE
-         * ->MSG
-         * ->MOREDETAILS
-         * ->MOREPARAMS
-        */
         if ($raw != "") {
-            $logs = [];
-            preg_match("/(" . self::START_DELIMITER . ")(.*\n*)*(" . self::END_DELIMITER . ")/g", $raw, $logs);
-            return ($logs);
+            $logs = explode(self::$options["logSeparator"], $raw);
+            array_pop($logs);
+            $ret = [];
+            foreach ($logs as $log) {
+                $matches = [];
+                preg_match_all("/([A-Z]+):(.*)\n/", $log, $matches);
+                $ret[] = new EHError(array_combine($matches[1], $matches[2]));
+            }
+
+            return ($ret);
         }
         return false;
     }
@@ -142,19 +144,20 @@ class ErrorHandler {
     }
 
     /**
-     * @param Exception $ex Exception capturée
+     * @param \Exception $ex Exception capturée
      * @param string $type Le type d'erreur (Utilisé pour créer des sous-répertoires pour catégoriser les logs)
+     * @param \DateTime|null $now La date de création du fichier de log (utilisé pour le nom)
      * @param mixed $additionalParams Paramètres supplémentaires que vous souhaitez y ajouter
      * @return string La chaîne de caractères formattée du log
      */
-    private static function formatError(Exception $ex, $type = "unknown", $additionalParams = null) {
+    private static function formatError(\Exception $ex, $type = "unknown", \DateTime $now = null, $additionalParams = null) {
         $moreDetails = method_exists($ex, "getMoreDetails");
         $err =
-            self::START_DELIMITER . PHP_EOL
-            . "DATE:"       . new MyDateTime() . PHP_EOL
+            "DATE:"       . $now->format(self::$options["formats"]["dateLog"]) . PHP_EOL
+            . "CODE:"       . $ex->getCode() . PHP_EOL
             . "HOST:"       . $_SERVER["HTTP_HOST"] . PHP_EOL
             . "TYPE:"       . $type . PHP_EOL
-            . "FILE:"       . $ex->getFile() .":". $ex->getLine() ."-". $ex->getCode() . PHP_EOL
+            . "FILE:"       . $ex->getFile() .":". $ex->getLine() . PHP_EOL
             . "MSG:"        . $ex->getMessage() . PHP_EOL;
         if ($moreDetails) {
             $err .= "MOREDETAILS:<pre>" . var_dump($ex->getMoreDetails) . "</pre>" . PHP_EOL;
@@ -163,6 +166,6 @@ class ErrorHandler {
             $err .= "ADDITIONALPARAMETERS:<pre>" . var_dump($additionalParams) . "</pre>" . PHP_EOL;
         }
 
-        return $err . self::END_DELIMITER . PHP_EOL;
+        return $err . self::$options["logSeparator"] . PHP_EOL;
     }
 }
